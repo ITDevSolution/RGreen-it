c5e6b769c51276da561b9d046bb84e6e0a3fec21
Joel <jronceros.byj@gmail.com>
2022-06-29 17:11:00 -0500

    Building PostBox component parte 2

--- components/PostBox.js
+++ components/PostBox.js
@@ -1,15 +1,25 @@
 import { useSession } from "next-auth/react"
 import React from "react"
 import Avatar from "./Avatar"
+import { LinkIcon, PhotographIcon } from "@heroicons/react/outline"
+import { useForm } from "react-hook-form"
 
 function PostBox() {
   const { data: session } = useSession()
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
   return (
-    <form>
+    <form className="z-50 rounded-md border border-gray-300 bg-white p-2">
       <div className="flex items-center space-x-3">
         {/* Avatar */}
         <Avatar />
         <input
+          {...register("post title", { required: true })}
           disabled={!session}
           className="flex-1 rounded-md bg-gray-50 p-2 outline-none"
           type="text"
@@ -17,7 +27,12 @@ function PostBox() {
             session ? "Create a post by entering a title!" : "Sign in to post"
           }
         />
+
+        {/* Icons  */}
+        <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+        <LinkIcon className="h-6 text-gray-300" />
       </div>
+      {!!watch("post title") && <div>{/* Body */}</div>}
     </form>
   )
 }
