06547ac0725c1e0b3d3c86dc4b0472f1ae4b12ef
Joel <jronceros.byj@gmail.com>
2022-06-29 22:19:30 -0500

    Building the body section of de PostBox and errors section

--- components/PostBox.js
+++ components/PostBox.js
@@ -1,11 +1,12 @@
 import { useSession } from "next-auth/react"
-import React from "react"
+import { useState } from "react"
 import Avatar from "./Avatar"
 import { LinkIcon, PhotographIcon } from "@heroicons/react/outline"
 import { useForm } from "react-hook-form"
 
 function PostBox() {
   const { data: session } = useSession()
+  const [imageBoxOpen, setImageBoxOpen] = useState(false)
   const {
     register,
     setValue,
@@ -18,8 +19,9 @@ function PostBox() {
       <div className="flex items-center space-x-3">
         {/* Avatar */}
         <Avatar />
+        {/* Input text */}
         <input
-          {...register("post title", { required: true })}
+          {...register("postTitle", { required: true })}
           disabled={!session}
           className="flex-1 rounded-md bg-gray-50 p-2 outline-none"
           type="text"
@@ -28,11 +30,73 @@ function PostBox() {
           }
         />
 
-        {/* Icons  */}
-        <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+        {/* Icons Input text */}
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 text-gray-300 cursor-pointer ${
+            imageBoxOpen && "text-blue-300"
+          }`}
+        />
         <LinkIcon className="h-6 text-gray-300" />
       </div>
-      {!!watch("post title") && <div>{/* Body */}</div>}
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          {/* Body */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postBody")}
+              type="text"
+              placeholder="Text (optional)"
+            />
+          </div>
+
+          {/* Subrredit */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("subreddit", { required: true })}
+              type="text"
+              placeholder="i.e. nextjs"
+            />
+          </div>
+          {/* Image URL */}
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register("postImage")}
+                type="text"
+                placeholder="optional..."
+              />
+            </div>
+          )}
+
+          {/* Erros */}
+          {Object.keys(errors).lentgth > 0 && (
+            <div className="space-y-2 p-2 text-red-500 ">
+              {errors.postTitle?.type === "required" && (
+                <p>A post title is required</p>
+              )}
+              {errors.subreddit?.type === "required" && (
+                <p>A Subreddit is required</p>
+              )}
+            </div>
+          )}
+
+          {!!watch("postTitle") && (
+            <button
+              type="submit"
+              className="w-full rounded-full bg-blue-400 p-2 text-white"
+            >
+              Create Post
+            </button>
+          )}
+        </div>
+      )}
     </form>
   )
 }
