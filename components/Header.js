c5df5e4853886e62a4183c398f3906356c52f710
Joel <jronceros.byj@gmail.com>
2022-06-29 22:09:25 -0500

    fix:bug

--- components/Header.js
+++ components/Header.js
@@ -20,8 +20,6 @@ import { signIn, signOut, useSession } from "next-auth/react"
 function Header() {
   const { data: session, status } = useSession()
 
-  if (status !== "authenticated") return null
-
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       {/* logo */}
