cee21470415a798836561972166064a3512dd0d0
Joel <jronceros.byj@gmail.com>
2022-07-07 18:49:51 -0500

    Implement let people make comments to comments

--- components/Comment.js
+++ components/Comment.js
@@ -1,8 +1,13 @@
 import Avatar from "./Avatar"
 
 import timeago from "lib/timeago"
+import { ChatAltIcon } from "@heroicons/react/outline"
 
-export default function Comment({ comment }) {
+import { useState } from "react"
+import NewComment from "./NewComment"
+
+export default function Comment({ comment, post }) {
+  const [showReply, setShowReply] = useState(false)
   return (
     <div className="relative flex items-center space-x-2 space-y-5">
       <hr className="absolute top-10 h-16 border left-7 z-0" />
@@ -10,14 +15,36 @@ export default function Comment({ comment }) {
         <Avatar seed={comment.author.name} />
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col ">
         <p className="py-2 text-xs text-gray-400">
           <span className="font-semibold text-gray-600">
             {comment.author.name}
           </span>{" "}
           • {timeago.format(new Date(comment.createdAt))}
         </p>
-        <p>{comment.content}</p>
+        <p className="">{comment.content}</p>
+        {showReply ? (
+          <>
+            <NewComment
+              className={`${!showReply && "hidden"}`}
+              comment={comment}
+              post={post}
+            />
+            <button
+              className="rounded-full bg-violet-800 p-2 text-white disabled:text-black disabled:font-semibold disabled:bg-gray-200"
+              onClick={() => setShowReply(false)}
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <div className="flex space-x-4 text-gray">
+            <div onClick={() => setShowReply(true)} className="postButtons">
+              <ChatAltIcon className="h-4 w-4" />
+              <p className="text-sm">Reply</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
